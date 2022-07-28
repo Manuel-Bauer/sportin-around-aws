66e082a10bc5d5ddf29ed3c53ed549e5e7766e53
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-07-28 19:33:01 +0200

    signin form states added

--- pages/SignIn.tsx
+++ pages/SignIn.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import { NextPage } from 'next';
+import { Amplify } from 'aws-amplify';
+
+import '@aws-amplify/ui-react/styles.css';
+
+// Built Sign In first with AWS components and them customize them
+
+// Built registration form
 
 const SignInPage: NextPage = () => {
+  /* INPUT STATES */
+  const [loginEmail, setLoginEmail] = useState<string>('');
+  const [loginPassword, setLoginPassword] = useState<string>('');
+  const [registrationEmail, setRegistrationEmail] = useState<string>('');
+  const [registrationPassword, setRegistrationPassword] = useState<string>('');
+  const [registrationConfirmPassword, setRegistrationConfirmPassword] =
+    useState<string>('');
+  const [verificationCode, setVerfificationCode] = useState<string>('');
+
   return (
     <>
-      <div>Hallo</div>;
+      <div>
+        <input type='text' />
+      </div>
     </>
   );
 };
